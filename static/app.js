deae714a6cce47538dc38e9ce575be30318a13f7
Mike Stepanovic <mrstepanovic@gmail.com>
2023-04-24 16:03:57 -0600

    changed bar plot to bubble plot. might try treemap next.

--- static/app.js
+++ static/app.js
@@ -20,11 +20,39 @@ function displayBlocks(blocks) {
   container.innerHTML = '';
 
   blocks.forEach(block => {
-      const blockDiv = document.createElement('div');
-      blockDiv.innerHTML = `Slot: ${block.slot}, Block Root: ${block.block_root}`;
-      container.appendChild(blockDiv);
+    const blockDiv = document.createElement('div');
+    blockDiv.innerHTML = `Slot: ${block.slot}, Block Root: ${block.block_root}`;
+
+    const metricsDiv = document.createElement('div');
+
+    const transactionHash = block.transactions[0].hash;
+    const epochNumber = block.slot;
+    const epochDatetime = new Date(block.timestamp * 1000);
+    const totalTransactions = block.transactions.length;
+    const totalEthTransacted = block.transactions.reduce((acc, tx) => acc + tx.value, 0);
+    const blockReward = 2; // Replace with actual block reward value
+    const averageEthPerTransaction = totalEthTransacted / totalTransactions;
+
+    metricsDiv.innerHTML = `
+      <strong>Transaction Hash:</strong> <a href="https://etherscan.io/tx/${transactionHash}" target="_blank">${transactionHash}</a><br>
+      <strong>Epoch Number:</strong> ${epochNumber}<br>
+      <strong>Epoch Datetime:</strong> ${epochDatetime}<br>
+      <strong>Total Transactions:</strong> ${totalTransactions}<br>
+      <strong>Total ETH Transacted:</strong> ${totalEthTransacted.toFixed(2)} ETH<br>
+      <strong>Block Reward:</strong> ${blockReward} ETH<br>
+      <strong>Average ETH per Transaction:</strong> ${averageEthPerTransaction.toFixed(2)} ETH
+    `;
+
+    container.appendChild(blockDiv);
+    container.appendChild(metricsDiv);
   });
+
+  // Pass the transactions from all blocks to the drawBubblePlot function
+  const allTransactions = blocks.flatMap(block => block.transactions);
+  drawBubblePlot(allTransactions);
 }
+
+
 
 async function submitHandler(event) {
   event.preventDefault();
@@ -42,9 +70,15 @@ async function submitHandler(event) {
 
   const blocks = await fetchBlocks(validatorIndex, epoch);
   displayBlocks(blocks);
-  drawChart(blocks);
+  drawBubblePlot(blocks);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('blocks-form').addEventListener('submit', submitHandler);
+});
+
+$(document).ready(function() {
+  $('#validator-index').tooltip({ content: 'Enter the validator index number.' });
+  $('#epoch').tooltip({ content: 'Enter the epoch number.' });
+  $('#epoch').inputmask('9{1,10}');
 });
--- static/d3_functions.js
+++ static/d3_functions.js
@@ -1,50 +1,89 @@
-function drawChart(blocks) {
-    const margin = { top: 20, right: 30, bottom: 40, left: 250 };
-    const width = 960 - margin.left - margin.right;
-    const height = 800 - margin.top - margin.bottom; // Increase the height
-  
-    const transactions = blocks.flatMap(block => block.transactions);
-  
-    transactions.sort((a, b) => b.value - a.value);
-  
-    const x = d3.scaleLinear()
-      .domain([0, d3.max(transactions, d => d.value)]).nice()
-      .range([margin.left, width - margin.right]);
-  
-    const y = d3.scaleBand()
-      .domain(transactions.map(d => d.hash))
-      .range([margin.top, height - margin.bottom])
-      .padding(0.3); // Adjust the padding between the bars
-  
-    const svg = d3.select("#results")
-      .append("svg")
-      .attr("viewBox", [0, 0, width, height]);
-  
-    svg.append("g")
-      .attr("fill", "red")
-      .selectAll("rect")
-      .data(transactions)
-      .join("rect")
-      .attr("x", x(0))
-      .attr("y", d => y(d.hash))
-      .attr("width", d => x(d.value) - x(0))
-      .attr("height", y.bandwidth())
-      .on("mouseover", function() { d3.select(this).attr("fill", "blue"); })
-      .on("mouseout", function() { d3.select(this).attr("fill", "red"); });
-  
-    svg.append("g")
-      .call(d3.axisLeft(y).tickSizeOuter(0))
-      .attr("transform", `translate(${x(0)},0)`);
-  
-    svg.append("g")
-      .call(d3.axisTop(x).ticks(width / 80))
-      .attr("transform", `translate(0,${margin.top})`);
-  
-    svg.append("text")
-      .attr("x", width / 2)
-      .attr("y", margin.top - 10)
-      .attr("text-anchor", "middle")
-      .style("font-size", "16px")
-      .text("ETH Value");
+// drawBubblePlot function to create a bubble plot visualization of transactions
+function drawBubblePlot(transactions) {
+  // Define dimensions, margins, and scales
+  const margin = { top: 20, right: 30, bottom: 40, left: 250 };
+  const width = 960 - margin.left - margin.right;
+  const height = 800 - margin.top - margin.bottom;
+
+  const maxTransactionValue = d3.max(transactions, d => d.value);
+  const minTransactionValue = d3.min(transactions, d => d.value);
+
+  const x = d3.scaleLinear()
+    .domain([0, maxTransactionValue]).nice()
+    .range([margin.left, width - margin.right]);
+
+  const y = d3.scaleLinear()
+    .domain([0, maxTransactionValue]).nice()
+    .range([height - margin.bottom, margin.top]);
+
+  const radius = d3.scaleSqrt()
+    .domain([0, maxTransactionValue])
+    .range([1, 30]);
+
+  // Color scale for bubbles based on ETH value
+  const color = d3.scaleSequential()
+    .domain([minTransactionValue, maxTransactionValue])
+    .interpolator(d3.interpolateViridis);
+
+  // Create the SVG element
+  const svg = d3.select("#results")
+    .append("svg")
+    .attr("viewBox", [0, 0, width, height]);
+
+  // Create a force simulation for bubble positioning
+  const simulation = d3.forceSimulation(transactions)
+    .force("charge", d3.forceManyBody().strength(5))
+    .force("x", d3.forceX().x(d => x(d.value)))
+    .force("y", d3.forceY().y(d => y(d.value)))
+    .force("collision", d3.forceCollide().radius(d => radius(d.value) + 1))
+    .on("tick", ticked);
+
+  function ticked() {
+    node.attr("cx", d => d.x)
+        .attr("cy", d => d.y);
   }
-  
+
+  // Create the bubbles (circle elements)
+  const node = svg.selectAll("circle")
+    .data(transactions)
+    .join("circle")
+    .attr("r", d => radius(d.value))
+    .attr("fill", d => color(d.value))
+    .attr("stroke", "black")
+    .attr("stroke-width", 1)
+    .on("mouseover", function() { d3.select(this).attr("stroke-width", 2); })
+    .on("mouseout", function() { d3.select(this).attr("stroke-width", 1); })
+    .on("click", d => window.open(`https://etherscan.io/tx/${d.hash}`, "_blank"));
+
+  // Add abbreviated transaction hash labels inside the bubbles
+  svg.selectAll("text")
+    .data(transactions)
+    .join("text")
+    .attr("x", d => x(d.value))
+    .attr("y", d => y(d.value))
+    .attr("text-anchor", "middle")
+    .attr("dy", ".35em")
+    .attr("font-size", "10px")
+    .attr("fill", "white")
+    .text(d => d.hash.substring(0, 6) + "...");
+
+  // Add X and Y axis labels
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", margin.top - 10)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .text("ETH Value");
+
+  // Add X and Y axes
+  const xAxis = d3.axisBottom(x).ticks(width / 80);
+  const yAxis = d3.axisLeft(y).ticks(height / 80);
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(yAxis);
+}
